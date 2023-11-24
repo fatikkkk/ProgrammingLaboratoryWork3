d375240ff724f1b7c842a42884674afa1f4e51b7
fatikkkk <98143123+fatikkkk@users.noreply.github.com>
2023-11-25 02:20:21 +0300

    almost full work

--- ProgrammingLaboratoryWork3/ProgrammingLaboratoryWork3.cpp
+++ ProgrammingLaboratoryWork3/ProgrammingLaboratoryWork3.cpp
@@ -1,20 +1,250 @@
-// ProgrammingLaboratoryWork3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <ctime>
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int sizeArray = 6;
+
+void showArray(int(*A)[sizeArray]) {
+    system("cls");
+    HANDLE hStdout;
+    COORD destCoord;
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    for (int i = 0; i < sizeArray; i++)
+        for (int j = 0; j < sizeArray; j++)
+        {
+            destCoord.X = i * 4;
+            destCoord.Y = j;
+            SetConsoleCursorPosition(hStdout, destCoord);
+            cout << A[i][j];
+        }
+    cout << "\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void fillSnake(int(*A)[sizeArray]) {
+    for (int i = 0; i < sizeArray; i++)
+        for (int j = 0; j < sizeArray; j++) {
+            A[i][j] = 0;
+        }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    showArray(A);
+
+    int* end = A[0] + sizeArray * sizeArray - 1;
+
+    HANDLE hStdout;
+    COORD destCoord;
+    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    for (int* pt = A[0], i = 0, x = 0, y = 0; pt <= end + (sizeArray - 1); pt++, i++, y++) {
+        if (i % sizeArray == 0 && i != 0) {
+            x++;
+            y = 0;
+            if (x % 2 == 0)
+                pt = &A[x][y];
+            else
+                pt = &A[x][y + sizeArray - 1];
+        }
+        if (x % 2 == 0) {
+            destCoord.X = x * 4;
+            destCoord.Y = y;
+            SetConsoleCursorPosition(hStdout, destCoord);
+            *pt = 1 + rand() % (sizeArray * sizeArray);
+            cout << *pt;
+            Sleep(50);
+        }
+        else {
+            destCoord.X = x * 4;
+            destCoord.Y = sizeArray - y - 1;
+            SetConsoleCursorPosition(hStdout, destCoord);
+            *(pt - y - y) = 1 + rand() % (sizeArray * sizeArray);
+            cout << *(pt - y - y);
+            Sleep(50);
+        }
+    }
+
+    cout << "\r";
+    for (int i = 1; i <= sizeArray; i++)
+        cout << "\n";
+    system("pause");
+
+}
+
+void fillArray(int(*B)[sizeArray]) {
+    for (int i = 0; i < sizeArray; i++)
+        for (int j = 0; j < sizeArray; j++) {
+            B[i][j] = 1 + rand() % (sizeArray * sizeArray);
+        }
+
+    showArray(B);
+
+    system("pause");
+}
+
+void shakeA(int(*A)[sizeArray]) {
+    for (int i = 0; i < (sizeArray / 2); i++) {
+        for (int j = 0; j < sizeArray; j++) {
+            swap(A[i][j], A[i + (sizeArray / 2)][j]);
+        }
+    }
+    for (int i = 0; i < (sizeArray / 2); i++) {
+        for (int j = 0; j < (sizeArray / 2); j++) {
+            swap(A[i][j], A[i + (sizeArray / 2)][j + (sizeArray / 2)]);
+        }
+    }
+}
+
+void shakeB(int(*A)[sizeArray]) {
+    for (int i = 0; i < (sizeArray / 2); i++) {
+        for (int j = 0; j < sizeArray; j++) {
+            if (j < (sizeArray / 2)) {
+                swap(A[i][j], A[i + (sizeArray / 2)][j + (sizeArray / 2)]);
+            }
+            else {
+                swap(A[i][j], A[i + (sizeArray / 2)][j - (sizeArray / 2)]);
+            }
+        }
+    }
+}
+
+void shakeC(int(*A)[sizeArray]) {
+    for (int i = 0; i < sizeArray; i++) {
+        for (int j = 0; j < (sizeArray / 2); j++) {
+            swap(A[i][j], A[i][j + (sizeArray / 2)]);
+        }
+    }
+}
+
+void shakeD(int(*A)[sizeArray]) {
+    for (int i = 0; i < (sizeArray / 2); i++) {
+        for (int j = 0; j < sizeArray; j++) {
+            swap(A[i][j], A[i + (sizeArray / 2)][j]);
+        }
+    }
+}
+
+void sort(int(*A)[sizeArray]) {
+    int* pt = A[0];
+    bool sorted = false;
+
+    while (sorted != true) {
+        sorted = true;
+        for (int i = 0; i < (sizeArray * sizeArray - 1); i++) {
+            if (*(pt + i) > *(pt + i + 1)) {
+                swap(*(pt + i), *(pt + i + 1));
+                sorted = false;
+            }
+        }
+    }
+}
+
+void sumArrays(int(*A)[sizeArray], int(*B)[sizeArray]) {
+    for (int i = 0; i < sizeArray; i++)
+        for (int j = 0; j < sizeArray; j++)
+            A[i][j] += B[i][j];
+
+    showArray(A);
+}
+
+
+int main() {
+    setlocale(0, "");
+    srand(time(0));
+
+    int selectUserNum, chooseUser;
+
+    int A[sizeArray][sizeArray];
+    int B[sizeArray][sizeArray];
+
+    /* Fill Snake Array*/
+    fillSnake(A);
+            
+    /* Shake Array*/
+    cout << "Please, choose a number for Shake:\n";
+    cout << "\t 1 - Shake A - Circular\n";
+    cout << "\t 2 - Shake B - Diagonal\n";
+    cout << "\t 3 - Shake C - Vertical Mirror\n";
+    cout << "\t 4 - Shake D - Horizontal Mirror\n";
+    cout << "Number: ";
+    cin >> chooseUser;
+    cout << "\n";
+    showArray(A);
+    cout << "\nBefore Shake!\n";
+    switch (chooseUser)
+    {
+    case 1:
+        shakeA(A);
+        break;
+
+    case 2:
+        shakeB(A);
+        break;
+
+    case 3:
+        shakeC(A);
+        break;
+
+    case 4:
+        shakeD(A);
+        break;
+    default:
+        break;
+    }
+    Sleep(5000);
+    system("cls");
+    showArray(A);
+    cout << "\nAfter Shake!\n";
+    system("pause");
+    
+
+    /* Sorting Array */
+    system("cls");
+    showArray(A);
+    Sleep(5000);
+    sort(A);
+    showArray(A);
+    cout << "\n";
+    system("pause");
+    
+
+    /* Mathematics Actions with Array */
+    int input;
+    string sign;
+    cout << "Write me actions with array ('+', '-', '*', '/') and number\n";
+    cout << "For example: '- 3'\n";
+    cin >> sign >> input;
+
+    if (sign == "+") {
+        for (int i = 0; i < sizeArray; i++)
+            for (int j = 0; j < sizeArray; j++)
+                A[i][j] += input;
+    }
+    else if (sign == "-") {
+        for (int i = 0; i < sizeArray; i++)
+            for (int j = 0; j < sizeArray; j++)
+                A[i][j] -= input;
+    }
+    else if (sign == "*") {
+        for (int i = 0; i < sizeArray; i++)
+            for (int j = 0; j < sizeArray; j++)
+                A[i][j] *= input;
+    }
+    else if (sign == "/") {
+        for (int i = 0; i < sizeArray; i++)
+            for (int j = 0; j < sizeArray; j++)
+                A[i][j] /= input;
+    }
+    else {
+        cout << "Incorrect value or sign";
+    }
+
+    system("cls");
+    showArray(A);
+    system("pause");
+
+    /* Individual Home Work - Sum two Arrays */
+    system("cls");
+    fillArray(B);
+    sumArrays(A, B);
+    cout << "\n";
+}
